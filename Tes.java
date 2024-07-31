540122f9ba5c21447bf12bfe48a26c0f111aca50
azizahicha <icha_azizah_33rpl@student.smktelkom-mlg.sch.id>
2024-07-31 14:54:30 +0700

    Add files via upload

---
+++ Tes.java
@@ -0,0 +1,11 @@
+public class Tes {
+public static void main(String[] args) {
+System.out.println("Ini File Tes"); 
+System.out.println("Halo");
+System.out.println("Nama:Icha Chayrany Azizah");
+System.out.println("Alamat:Ds.Jatimulyo Kec.Kauman Kab.Tulungagung");
+System.out.println("Smp:SMP N 1 KAUMAN");
+System.out.println("No Hp:081515903078");
+}
+
+}
